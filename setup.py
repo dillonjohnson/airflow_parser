0db5c7b85cfa14e16f71d451808dc50341fd04d1
Dillon Johnson <djrepec@gmail.com>
2021-03-23 20:01:01 -0400

    Move subfolders

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name = 'airflow_parser',         # How you named your package folder (MyLib)
-    packages = ['airflow_parser'],   # Chose the same as "name"
+    packages = ['airflow_parser', 'airflow_parser.providers'],   # Chose the same as "name"
     version = '0.1',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Parses over control files to create dynamic DAGs.',   # Give a short description about your library
@@ -11,7 +11,8 @@ setup(
     download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz', # todo   # I explain this later on
     keywords = ['apache-airflow', 'parser'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
-        'apache-airflow'
+        'apache-airflow',
+        'xmltodict'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
